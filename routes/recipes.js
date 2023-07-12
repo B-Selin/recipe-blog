3a0051999ab3af07c4df45fafa1ab8633cda6746
B-Selin <selin@bozkurt.ca>
2023-07-11 18:00:35 -0600

    style footer

---
+++ routes/recipes.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+// // You'll be creating this controller module next
+// const recipesCtrl = require("../controllers/recipes");
+// // Require the auth middleware
+// const ensureLoggedIn = require("../config/ensureLoggedIn");
